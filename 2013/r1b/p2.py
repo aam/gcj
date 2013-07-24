8a0f164771317b5d165d71cb22c77d7013fc5a2c
Alexander Aprelev <alexander@aprelev.com>
2013-07-24 19:42:33 -0400

    WIP

--- 2013/r1b/p2.py
+++ 2013/r1b/p2.py
@@ -11,40 +11,36 @@ with open('problem2') as f:
 
 
     i = (1.+math.sqrt(1.+8.*N))/4. - 1.
-    si = math.floor(i) * 2
-    ei = si + 2
-    print "need to look between floor(%s)=%d and ceil(%s)=%d" % (i, si, i, ei)
+    si = math.floor(i) * 2 # height of last fully completed triangle
+    ei = si + 2 # height of outer triangle
+    print "need to look between si=%d and ei=%d" % (si, ei)
 
-
-    if (X < -ei or X > ei or
-       Y > ei - X and X >= 0 or 
-       Y > X + ei and X < 0):
-      print "no chance X=%d Y=%d ei=%d" % (X, Y, ei)
+    if (Y <= si - abs(X)): 
+      print "for sure (X,Y)=(%d, %d) inside of inner triangle since Y=%d <= si=%d - abs(X)=%d" % (X, Y, Y, si, abs(X))
+      result = 1.
+    elif (Y > ei - abs(X)):
+      print "no chance since (X,Y)=(%d, %d) outside of outer triangle since Y=%d > ei=%d - abs(X)=%d" % (X, Y, Y, ei, abs(X))
       result = 0.
     else:
-      if (X < 0 and Y < ei - X or
-         X >= 0 and Y < X + ei):
-        print "for sure"
+      print "now (X,Y)=(%d, %d) should be on outer triangle side" % (X, Y)
+      lastOne = 2 * si * si - si
+      nDiamonds = N - lastOne
+      if nDiamonds == 0:
         result = 1.
       else:
-        lastOne = 2 * ei * ei - ei
-        nDiamonds = N - lastOne
-        if nDiamonds == 0:
-          result = 1.
-        else:
-          capacity = ei + 1
-          overflow = max(math.ceil(nDiamonds/2) - capacity, 0)
-          print "lastOne=%d nDiamonds=%d ei=%d capacity=%d overflow=%d" % (lastOne, nDiamonds, ei, capacity, overflow)
+        capacity = si + 1
+        overflow = max(math.ceil(nDiamonds/2) - capacity, 0)
+        print "lastOne=%d nDiamonds=%d si=%d capacity=%d overflow=%d" % (lastOne, nDiamonds, si, capacity, overflow)
 
-          if Y < overflow:
-            result = 1.
-          elif Y > nDiamonds:
-            result = 0.
-          else:
-            capacityInPlay = capacity - overflow
-            diamondsInPlay = nDiamonds - overflow * 2
-            print "\tcapacityInPlay=%d diamondsInPlay=%d" % (capacityInPlay, diamondsInPlay)
-            result = (diamondsInPlay - (Y - overflow) - 1) / (diamondsInPlay - (Y - overflow))
+        if Y < overflow:
+          result = 1.
+        elif Y > nDiamonds:
+          result = 0.
+        else:
+          capacityInPlay = capacity - overflow
+          diamondsInPlay = nDiamonds - overflow * 2
+          print "\tcapacityInPlay=%d diamondsInPlay=%d" % (capacityInPlay, diamondsInPlay)
+          result = (diamondsInPlay - (Y - overflow) - 1.) / (diamondsInPlay - (Y - overflow))
 
     print "Case #%d: %d" % (iT + 1, result)
     iT += 1
